72a85c485229d05d6036c00388d1455bbb80d3c1
prrThr <arthur.op9@gmail.com>
2023-09-01 17:46:28 -0300

    updated

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,68 +1,70 @@
 const People = require('../../models/People');
 
+
 async function importarDados() {
-    const fs = require('fs');
-    const readline = require('readline');
-    const filePath = 'people-100000.xlsx';
-    const fileStream = fs.createReadStream(filePath);
-    
-    const rl = readline.createInterface({
+  const fs = require('fs');
+  const readline = require('readline');
+  const filePath = 'src/utils/people-100000.csv';
+  const fileStream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity // Detecta automaticamente a quebra de linha (CR+LF ou LF)
-    });
+  });
 
-    let primeiraLinha = true;
-    
-    rl.on('line', async (line) => {
-      if (primeiraLinha){
-        primeiraLinha = false;
-      }
-      const campos = line.split(',');
-      const id = campos[0];
-      const nome = campos[1];
-      console.log(campos[1]);
-      const sobrenome = campos[2];
-      const sexo = campos[3];
-      const em = campos[4];
-      const telefone = campos[5];
-      const nascimento = campos[6];
-      const profissao = campos[7];
-  
-      let person = {
-        userId: id,
-        firstName: nome,
-        lastName: sobrenome,
-        sex: sexo,
-        email: em,
-        phone: telefone,
-        dateOfBirth: nascimento,
-        jobTitle: profissao};
-        
-        let returnedObject = await People.create(person);
-        let generatedKey = returnedObject.dataValues.index;
-        console.log("Sucesso! Generated key", generatedKey);
-  
-      console.log('Linha:', line);
-    });
-  
-    rl.on('close', () => {
-    console.log('Dados importados com sucesso!.');
-    });
-  }
+  let primeiraLinha = true;
 
-async function mostrarDados() {
-    let customers = await Customer.findAll();
-    for (const customer of customers) {
-        const selectedStore1 = await Store.findByPk(customer.store_id, { logging: false });
-        console.log()
+  rl.on('line', async (line) => {
+    if (primeiraLinha) {
+      primeiraLinha = false;
+    }
+    const campos = line.split(',');
+    const id = campos[1];
+    const nome = campos[2];
+    const sobrenome = campos[4];
+    const sexo = campos[5];
+    const em = campos[6];
+    const telefone = campos[7];
+    const nascimento = campos[8];
+    const profissao = campos[9];
+
+    console.log("Profissão: ", campos[9]);
+    let person = {
+      userId: id,
+      firstName: nome,
+      lastName: sobrenome,
+      sex: sexo,
+      email: em,
+      phone: telefone,
+      dateOfBirth: nascimento,
+      jobTitle: profissao
     };
+
+    let returnedObject = await People.create(person);
+    let generatedKey = returnedObject.dataValues.index;
+    console.log("Sucesso! Generated key", generatedKey);
+
+    console.log('Linha:', line);
+  });
+
+  rl.on('close', () => {
+    console.log('Dados importados com sucesso!.');
+  });
 }
 
-async function PesquisarDados(){
+async function mostrarDados() {
+  let customers = await Customer.findAll();
+  for (const customer of customers) {
+    const selectedStore1 = await Store.findByPk(customer.store_id, { logging: false });
+    console.log()
+  };
+}
+
+async function PesquisarDados() {
 }
 
 module.exports = {
-    importarDados,
-    mostrarDados,
-    PesquisarDados
+  importarDados,
+  mostrarDados,
+  PesquisarDados
 }
