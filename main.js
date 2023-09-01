2954bc32ea616d0ecf6596e2c04d9b20d1b2a306
prrThr <arthur.op9@gmail.com>
2023-09-01 16:54:29 -0300

    xlsx added

--- main.js
+++ main.js
@@ -1,39 +1,62 @@
+const prompt = require('prompt-sync')({ sigint: true });
+const functions = require('./src/utils/functions');
 //Criar tabela com as colunas: Index, UserId, FirstName, LastName, Sex, Email, Phone, DateBirth, Job Title
 
+// ---------------------------------------------- //
 
+async function main() {
+  let option = 0;
 
+  while (option != 8) {
+      option = 0;
+      while(option < 1 || option > 8) {
+          console.log("---Menu---")
+          console.log("1 - Mostrar todos os dados")
+          console.log("2 - Pesquisar dados")
+          console.log('3 - Limpar tela');
+          console.log('4 - Sair');
+          option = parseInt(prompt('Selecione uma opção: '));
+      }
+      
+      switch (option) {
+          case 1:
+              try {
+                  await functions.mostrarDados();    
+              } catch (error) {
+                  console.log("Error log: ", error);
+              }
+              break;
+  
+          // ------------------------------------------------------- //
+  
+          case 2:
+              try {
+                  await functions.PesquisarDados();
+              } catch (error) {
+                  console.log("Error log: ", error);
+              }
+              break;
+  
+          // ------------------------------------------------------- //
 
+          case 3:
+              console.clear();
+              break;
 
-async function menu() {
-  const prompt = require('prompt-sync')({ sigint: true });
-  let opc = "0";
-  while (opc != "3") {
-    console.log("---Menu---")
-    console.log("1.Mostrar todos os dados")
-    console.log("2.Pesquisar dados")
-    console.log("3.Sair")
-    opc = prompt("Escolha uma das opcoes: ");
+          // ------------------------------------------------------- //
 
-    switch (opc) {
-      case "1":
-        await mostrarDados();
-        break;
-
-      case "2":
-        await PesquisarDados();
-        break;
-
-      case "3":
-        console.log("saindo...")
-        break;
-
-      default:
-        console.log("Opção inválida. Por favor, escolha uma opção válida.");
-        break;
-    }
+          case 4:
+              console.log('Saindo...');
+              break;
+          
+          // ------------------------------------------------------- //
+          
+          default:
+              console.log("Opção inválida. Por favor, escolha uma opção válida.");
+      }
   }
 }
 
+main();
 testConnection();
 importarDados();
-menu();
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,33 +3,49 @@ const People = require('../../models/People');
 async function importarDados() {
     const fs = require('fs');
     const readline = require('readline');
-    const filePath = 'people-1000000.xlsx';
+    const filePath = 'people-100000.xlsx';
     const fileStream = fs.createReadStream(filePath);
     const rl = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity // Detecta automaticamente a quebra de linha (CR+LF ou LF)
+    input: fileStream,
+    crlfDelay: Infinity // Detecta automaticamente a quebra de linha (CR+LF ou LF)
     });
-    rl.on('line', (line) => {
-        console.log('Linha:', line);
+    let primeiraLinha = true;
+    rl.on('line', async (line) => {
+      if (primeiraLinha){
+        primeiraLinha = false;
+      }
+      const campos = line.split(',');
+      const id = campos[0];
+      const nome = campos[1];
+      console.log(campos[1]);
+      const sobrenome = campos[2];
+      const sexo = campos[3];
+      const em = campos[4];
+      const telefone = campos[5];
+      const nascimento = campos[6];
+      const profissao = campos[7];
+  
+      let person = {
+        userId: id,
+        firstName: nome,
+        lastName: sobrenome,
+        sex: sexo,
+        email: em,
+        phone: telefone,
+        dateOfBirth: nascimento,
+        jobTitle: profissao};
+        
+        let returnedObject = await Table.create(person);
+        let generatedKey = returnedObject.dataValues.index;
+        console.log("Sucesso! Generated key", generatedKey);
+  
+      console.log('Linha:', line);
     });
+  
     rl.on('close', () => {
-        console.log('Leitura concluída.');
+    console.log('Dados importados com sucesso!.');
     });
-
-    let person = {
-        index: ,
-        userId: ,
-        firstName: ,
-        lastName: ,
-        sex: ,
-        email: ,
-        phone: ,
-        dateOfBirth: ,
-        jobTitle: };
-    let returnedObject = await People.create(person);
-    let generatedKey = returnedObject.dataValues.index;
-    console.log("Sucesso! Generated key", generatedKey);
-}
+  }
 
 async function mostrarDados() {
     let customers = await Customer.findAll();
@@ -37,4 +53,13 @@ async function mostrarDados() {
         const selectedStore1 = await Store.findByPk(customer.store_id, { logging: false });
         console.log()
     };
+}
+
+async function PesquisarDados(){
+}
+
+module.exports = {
+    importarDados,
+    mostrarDados,
+    PesquisarDados
 }
